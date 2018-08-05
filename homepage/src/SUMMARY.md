86e47902290ae252307be4bf56b3485ded66c9fa
Bradley Bridges <blbridges96@gmail.com>
2018-08-05 17:41:44 -0500

    Add base of the image rec guide

--- homepage/src/SUMMARY.md
+++ homepage/src/SUMMARY.md
@@ -2,4 +2,5 @@
 
 [UAV Austin Documentation](README.md)
 
-[Python Guide](guides/python/index.html)
+- [Python Guide](guides/python/index.html)
+- [Image Recognition Primer](guides/image-rec/index.html)
--- scripts/build.sh
+++ scripts/build.sh
@@ -11,12 +11,14 @@ cd $(dirname "$0")"/.."
 # Build the individual books.
 (cd homepage && mdbook build)
 (cd guides/python && mdbook build)
+(cd guides/image-rec && mdbook build)
 
 # Move everything into a master book directory.
 cp -R homepage/book book
 rm -r book/guides
 mkdir book/guides
 cp -R guides/python/book book/guides/python
+cp -R guides/image-rec/book book/guides/image-rec
 
 touch book/.nojekyll
 cp CNAME LICENSE book/
