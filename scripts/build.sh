31090fbee37c7b92389949c3d1d43809dee5dd37
Bradley Bridges <blbridges96@gmail.com>
2018-08-06 18:12:18 -0500

    Rename homepage book to home

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,14 +9,14 @@ fi
 cd $(dirname "$0")"/.."
 
 # Build the individual books.
-(cd homepage && mdbook build)
+(cd home && mdbook build)
 (cd guides/python && mdbook build)
 (cd guides/image-rec && mdbook build)
 
 # Move everything into a master book directory.
 mkdir -p book
 rm -r book/*
-cp -a homepage/book/. book/
+cp -a home/book/. book/
 rm -r book/guides
 mkdir book/guides
 cp -R guides/python/book book/guides/python
